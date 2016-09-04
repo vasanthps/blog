01d1255ee0111b32c2b911e92c4302afd1226627
Vasanth Sriram <vasanth.sriram@rentomojo.com>
2016-09-05 02:26:48 +0530

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    
+    clean: ['dist'],
+
+    ejs: {
+      all: {
+        options: {
+          // site-wide vars here
+        },
+        src: ['**/*.ejs', '!node_modules/**/*', '!_*/**/*'],
+        dest: 'dist/',
+        expand: true,
+        ext: '.html',
+      },
+    },
+
+    copy: {
+      all: {
+        src: ['*.css', '*.html', 'images/**/*', 'img/**/*', '!Gruntfile.js', '*.ico'],
+        dest: 'dist/',
+      },
+    },
+
+    connect: {
+      options: {
+        port: process.env.PORT || 3131,
+        base: 'dist/',
+      },
+
+      all: {},
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+
+      html: {
+        files: '<%= ejs.all.src %>',
+        tasks: ['ejs'],
+      },
+
+      js: {
+        files: '<%= browserify.all.src %>',
+        tasks: ['browserify'],
+      },
+
+      assets: {
+        files: ['assets/**/*', '*.css', '*.js', 'images/**/*', 'img/**/*', '!Gruntfile.js'],
+        tasks: ['copy'],
+      }
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'dist/'
+      },
+      src: ['**/*']
+    }
+  });
+
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  
+  grunt.registerTask('default', ['clean', 'ejs', 'copy']);
+  
+  grunt.registerTask('server', ['default', 'connect', 'watch']);
+
+  grunt.registerTask('deploy', ['default', 'gh-pages']);
+
+};
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Blog
+> Personal blog site
+
+## grunt-static-site usage:
+Initialize the git repository for the project:
+```
+git init
+```
+
+Add and commit the files:
+```
+git add .
+git commit -m 'initial commit'
+```
+
+[Create a new repository on github](http://github.com/new)
+
+Make sure it has the same name you entered when you generated this project: Blog
+
+Edit the project files to add your content.
+
+Deploy to GitHub Pages:
+```
+grunt deploy
+```
